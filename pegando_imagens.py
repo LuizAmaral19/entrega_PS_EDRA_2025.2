6da60d1b4b46fb1f99407926cb4b9e2e0b3c005c
LuizAmaral19 <luizhsamaral@gmail.com>
2025-12-06 00:27:11 -0300

    First commit

--- pegando_imagens.py
+++
@@ -1,6 +0,0 @@
-from tello_sim_EDRA import Simulator
-
-meu_Tello = Simulator()
-
-meu_Tello.takeoff()
-meu_Tello.land()
--- treasure_hunting.py
+++ treasure_hunting.py
@@ -1,3 +1,17 @@
+#   -----------------------------------------------------------------------------
+#   Projeto:        Projeto Trainee - Processo Seletivo EDRA 2025.2
+#   Descrição:      O projeto do PS tem como objetivo a programação de um drone de treinamento simulado 
+#                   que deve percorrer um mapa de 400x400cm e coletar 5 tesouros distribuídos
+#                   aleatoriamente no mapa a cada execução do código e retornar ao ponto de início
+#                   após coletar todos os tesouros. Foi usada uma biblioteca TelloSim adaptada para
+#                   o PS, disponível no link: https://github.com/fabinsz/tello_sim_EDRA. Para mais
+#                   informações olhar os PDFs de edital presentes nesta pasta "tello_sim_EDRA-main".
+#   Autor:          Luiz Henrique da Silva Amaral
+#   Data:           05/12/2025
+#   Contato:        luizhsamaral@gmail.com
+#   GitHub:         https://github.com/LuizAmaral19 
+#   -----------------------------------------------------------------------------
+
 from tello_sim_EDRA import Simulator
 
 meu_Tello = Simulator()
@@ -14,6 +28,7 @@ global_dsl = -meu_Tello.field_limit  # Global Down Side Limit
 
 # Coordenadas úteis de cada célula
 cell1_rsl = meu_Tello.field_limit - fov_Tello   # Cell 1 Right Side Limit
+cell1_usl = meu_Tello.field_limit - fov_Tello   # Cell 1 Upper Side Limit
 cell1_dsl = fov_by_2                            # Cell 1 Down Side Limit
 
 cell2_rsl = -fov_by_2                           # Cell 2 Right Side Limit
@@ -30,7 +45,7 @@ cell3_dsl = -meu_Tello.field_limit + fov_Tello  # Cell 3 Down Side Limit
 def sweeping_cell1():
     conditional_cell1_movements = []
 
-    height_cell1 = global_usl - cell1_dsl
+    height_cell1 = cell1_usl - cell1_dsl
     step = fov_Tello
 
     full_sweeps = int(height_cell1 // step)
@@ -48,7 +63,7 @@ def sweeping_cell1():
         return max(0.0, abs(global_dsl - meu_Tello.cur_loc[1]) - fov_by_2 + overlap)
 
 
-    for c in range(full_sweeps -1):
+    for c in range(full_sweeps):
         if (c % 2) == 0:
             conditional_cell1_movements.append((meu_Tello.back, step)) 
             conditional_cell1_movements.append((meu_Tello.right, go_right)) 
@@ -59,9 +74,9 @@ def sweeping_cell1():
     if remainder > 0:
         conditional_cell1_movements.append((meu_Tello.back, remainder + overlap))
         if (full_sweeps % 2) == 0:
+            conditional_cell1_movements.append((meu_Tello.right, go_right))
             conditional_cell1_movements.append((meu_Tello.left, go_left))
         else:
-            conditional_cell1_movements.append((meu_Tello.right, go_right))
             conditional_cell1_movements.append((meu_Tello.left, go_left))
 
     conditional_cell1_movements.append((meu_Tello.back, go_cell2))
@@ -167,10 +182,12 @@ def sweeping_cell3():
 
     return conditional_cell3_movements
 
+
 movements_cell3 = [
     (meu_Tello.forward, lambda: abs(cell3_usl - meu_Tello.cur_loc[1]) - fov_by_2 + overlap)
 ]
 movements_cell3.extend(sweeping_cell3())
+
 
 def move_and_check(drone, command, distance):
 
